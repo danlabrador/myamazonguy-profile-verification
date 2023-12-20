10fb05c9e3ca2cb3e5859c74f61dd32803a9e07f
Dan Labrador <thonydaniellelabrador@gmail.com>
2023-12-20 14:57:58 -0500

    fix: correct logic

--- automated_verification.py
+++ automated_verification.py
@@ -1,23 +1,28 @@
 import csv
 
 def check_name_in_email(name, email):
-    name_parts = name.lower().split()
-    email = email.lower()
+    name_parts = name.strip().lower().split()
+    email = email.strip().lower()
+    
+    if not email:
+        return False  # Skip records with no email
+    
     return any(part in email for part in name_parts)
 
 def process_csv(file_path, output_file_path):
-    with open(file_path, newline='') as csvfile, open(output_file_path, 'w', newline='') as outfile:
+    with open(file_path, mode='r', encoding='utf-8', newline='') as csvfile, open(output_file_path, 'w', encoding='utf-8', newline='') as outfile:
         reader = csv.DictReader(csvfile)
         fieldnames = reader.fieldnames + ['Result']
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in reader:
+            name = (row['First Name'] + ' ' + row['Last Name']).strip()  # Combine and strip spaces
             email = row['Email Address']
-            if email:  # Only process rows where email is not empty
-                first_name = row['First Name']
-                result = check_name_in_email(first_name, email)
-                row['Result'] = 'Pass' if result else 'Fail'
-                writer.writerow(row)
+            if email:
+                result = check_name_in_email(name, email)
+                if not(result):
+                    row['Result'] = ''
+                    writer.writerow(row)
 
 # Change this to the path of your CSV file
 file_path = 'data.csv'
